76d2525d46c1ccfa23c043b507d049f2bc73ddc9
monmondesu <100749524+monmondesu@users.noreply.github.com>
2022-03-09 13:59:30 +0900

    second commit

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import datetime as datetime
 import pandas_datareader
-import datetime
 import plotly.graph_objects as go
 import sklearn
 import sklearn.linear_model
@@ -11,16 +10,17 @@ import sklearn.model_selection
 from PIL import Image
 import yfinance as yf
 
-st.title("AIで株価予測アプリ")
+st.title('AIで株価予測アプリ')
 st.write('AIを使って、株価を予測してみましょう。')
 
 image = Image.open('stock_predict.png')
 st.image(image, use_column_width=True)
 
-st.write('あくまでAIによる予測です（参考値）。こちらのアプリによる損害や損失は一切補償しかねます。')
+st.write('※あくまでAIによる予測です（参考値）。こちらのアプリによる損害や損失は一切補償しかねます。')
 
-st.header("株価銘柄のティッカーシンボルを入力してください。")
-stock_name = st.text_input("例：AAPL,　FB, SFTBY　（大文字・小文字どちらでも可）", "AAPL")
+st.header('株価銘柄のティッカーシンボルを入力してください。')
+
+stock_name = st.text_input("例:AAPL, FB, SFTBY（大文字、小文字どちらでも可）", "AAPL")
 
 stock_name = stock_name.upper()
 
@@ -32,18 +32,18 @@ st.markdown(link)
 st.write('ティッカーシンボルについては上のリンク（SBI証券）をご参照ください')
 
 try:
-    df_stock =  pandas_datareader.data.DataReader(stock_name, 'yahoo','2020-01-01')
+    df_stock = pandas_datareader.data.DataReader(stock_name, 'yahoo', '2020-01-01')
 
-    st.header(stock_name_full + "2021年1月1日から現在までの価格（USD）")
+    st.header(stock_name_full + " 2020年1月1日から現在までの価格（USD）")
     st.write(df_stock)
 
-    st.header(stock_name_full + "終値と14間平均（USD）")
+    st.header(stock_name_full + " 終値と14日間平均（USD）")
     df_stock['SMA'] = df_stock['Close'].rolling(window=14).mean()
-    df_stock2 = df_stock[['Close','SMA']]
+    df_stock2 = df_stock[['Close', 'SMA']]
     st.line_chart(df_stock2)
 
-    st.header(stock_name_full + "値動き（USD）")
-    df_stock['change'] = (((df_stock['Close'] - df_stock['Open'])) / (df_stock['Open']) *100)
+    st.header(stock_name_full + " 値動き（USD）")
+    df_stock['change'] = (((df_stock['Close'] - df_stock['Open'])) / (df_stock['Open']) * 100)
     st.line_chart(df_stock['change'].tail(100))
 
     fig = go.Figure(
@@ -53,18 +53,19 @@ try:
             high = df_stock['High'],
             low = df_stock['Low'],
             close = df_stock['Close'],
-        increasing_line_color = 'green',
-        decreasing_line_color = 'red',
+            increasing_line_color = 'green',
+            decreasing_line_color = 'red',
             )
         ]
     )
 
-    st.header(stock_name_full + "キャンドルスティック")
+    st.header(stock_name_full + " キャンドルスティック")
     st.plotly_chart(fig)
 
-    df_stock['label'] = df_stock['Close'].shift(-38)
 
-    st.header(stock_name_full + '1か月後を予測しよう（USD）')
+    df_stock['label'] = df_stock['Close'].shift(-30)
+
+    st.header(stock_name_full + ' 1か月後を予測しよう（USD）')
     def stock_predict():
         X = np.array(df_stock.drop(['label', 'SMA'], axis=1))
         X = sklearn.preprocessing.scale(X)
@@ -72,9 +73,7 @@ try:
         X = X[:-30]
         y = np.array(df_stock['label'])
         y = y[:-30]
-
         X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
-
         model = sklearn.linear_model.LinearRegression()
         model.fit(X_train, y_train)
 
@@ -88,7 +87,7 @@ try:
             st.write('信頼度：中')
         else:
             st.write('信頼度：低')
-        st.write('オレンジの線(Predict）が予測値です。')
+        st.write('オレンジ色の線（Predict）が予測値です。')
 
         predicted_data = model.predict(predict_data)
         df_stock['Predict'] = np.nan
@@ -99,10 +98,10 @@ try:
         for data in predicted_data:
             next_date = datetime.datetime.fromtimestamp(next_unix)
             next_unix += one_day
-            df_stock.lock[nexr_date] = np.append([np.nan]* (len(df_stock.columns)-1), data)
+            df_stock.loc[next_date] = np.append([np.nan] * (len(df_stock.columns) - 1), data)
 
-        df_stock['Close'].plot(figsize=(15,6), color="green")
-        df_stock['Predict'].plot(figsize=(15,6), color="orange")
+        df_stock['Close'].plot(figsize=(15, 6), color='green')
+        df_stock['Predict'].plot(figsize=(15, 6), color='orange')
 
         df_stock3 = df_stock[['Close', 'Predict']]
         st.line_chart(df_stock3)
@@ -114,4 +113,14 @@ except:
     st.error(
         "エラーがおきているようです。"
     )
-st.write('Copyright © 2021 Tomoyuki Yoshikawa. All Rights Reserved.')
+
+st.write('Copyright © 2021 Tomoyuki Yoshikawa. All Rights Resereved.')
+
+
+
+
+
+
+
+
+
